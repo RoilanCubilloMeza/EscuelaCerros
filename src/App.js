bd9141c1a70de2144070d37987bd76dc9fc3b9c2
RoilanCubillo <andreycubillo69@gmail.com>
2023-07-24 20:17:58 -0600

    integracion de login, navbar y register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import Login from './Pages/Login';
+import Registration from './Pages/Register';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
- 
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <Route path="/login" element={<Login/>} />
+    <Route path="/register" element={<Registration/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Register.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+const Registration = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Aquí puedes agregar cualquier lógica adicional, como validación de datos, antes de enviarlos al servidor.
+
+    // Ejemplo de cómo mostrar los datos ingresados en la consola.
+    console.log('Datos del formulario:', formData);
+
+    // Aquí puedes enviar los datos al servidor utilizando fetch() o cualquier otra técnica para manejar el registro de usuarios.
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Registro de Usuario</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">Nombre de usuario:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Correo electrónico:</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Contraseña:</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Registrarse</button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;

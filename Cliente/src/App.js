bdd0420f1dcc0a166b04a39bc08de847d869266a
RoilanCubillo <roilan.cubillo.meza@est.una.ac.cr>
2024-04-28 22:27:15 -0600

    Implementación Protección rutas.

--- Cliente/src/App.js
+++ Cliente/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { UserProvider } from "./components/UserContext";
+import { ThemeProvider } from "./components/Theme";
 import Navbar from "./components/Navbar";
 import Login from "./Pages/Login";
 import Registration from "./Pages/Register";
@@ -24,48 +26,54 @@ import Usuarios from "./Pages/Usuarios";
 import Noticias from "./Pages/Noticias";
 import Roles from "./Pages/Roles";
 import Parentesco from "./Pages/Perentesco";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "./components/Theme";
 import Horarios from "./Pages/Horarios";
 import Examen from "./Pages/Examen";
 import Justificacion from "./Pages/Justificacion";
+
 function App() {
+  const [refresh, setRefresh] = useState(false);
+  const isAuthenticated = localStorage.getItem("token") !== null;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRefresh(prevRefresh => !prevRefresh);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <BrowserRouter>
       <UserProvider>
         <ThemeProvider>
-          <Navbar/>
+          <Navbar />
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Registration />} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/Estudiantes" roles={[1, 2, 3]} element={<Persona />} />
-            <Route path="/Encargado" roles={[1, 2, 3]} element={<Encargado />} />
-            <Route path="/Enfermedades" roles={[1, 2, 3]} element={<Enfermedades />} />
-            <Route path="/Escolaridad" roles={[1, 2, 3]} element={<Escolaridad />} />
-            <Route path="/Adecuacion" roles={[1, 2, 3]} element={<Adecuacion />} />
-            <Route path="/Ocupacion" roles={[1, 2, 3]} element={<Ocupacion />} />
-            <Route path="/LugarResidencia" roles={[1, 2, 3]} element={<LugarResidencia />} />
-            <Route path="/Grado" roles={[1, 2, 3]} element={<Grado />} />
-            <Route path="/Materias" roles={[1, 2, 3]} element={<Materias />} />
-            <Route path="/Examen" element={<Examen/>}/>
-            <Route path="/Tareas" roles={[1, 2, 3]} element={<Tareas />} />
-            <Route path="/Cotidiano" roles={[1, 2, 3]} element={<Cotidiano />} />
-            <Route path="/Asistencia" roles={[1, 2, 3]} element={<Asistencia />} />
-            <Route path="/Matricula" roles={[1, 2, 3]} element={<Matricula />} />
-
-            <Route path="/AdminDashboard" roles={[1]} element={<AdminDashboard />} />
-            <Route path="/ProfesorDashboard" roles={[2]} element={<ProfesorDashboard />} />
-            <Route path="/EstudianteDashboard" roles={[3]} element={<EstudianteDashboard />} /> 
-            
-            <Route path="/Usuarios" roles={[1, 2, 3]} element={<Usuarios/>}/>
-            <Route path="/Roles" roles={[1, 2, 3]} element={<Roles/>}/>
-            <Route path="/Noticias" roles={[1, 2, 3]} element={<Noticias/>}/>
-            <Route path="/Parentesco" roles={[1, 2, 3]} element={<Parentesco/>}/>
-            <Route path="/Horarios" element={<Horarios/>}/>
-            <Route path="/Justificacion" element={<Justificacion/>}/>
-
+            <Route path="/" element={<Home />} />
+            <Route path="/Horarios" element={<Horarios />} />
+            <Route path="/Estudiantes" element={isAuthenticated ? <Persona /> : <Navigate to="/Persona" replace />} />
+            <Route path="/Encargado" element={isAuthenticated ? <Encargado /> : <Navigate to="/Encargado" replace />} />
+            <Route path="/Enfermedades" element={isAuthenticated ? <Enfermedades /> : <Navigate to="/login" replace />} />
+            <Route path="/Escolaridad" element={isAuthenticated ? <Escolaridad /> : <Navigate to="/Enfermedades" replace />} />
+            <Route path="/Adecuacion" element={isAuthenticated ? <Adecuacion /> : <Navigate to="/Adecuacion" replace />} />
+            <Route path="/Ocupacion" element={isAuthenticated ? <Ocupacion /> : <Navigate to="/Ocupacion" replace />} />
+            <Route path="/LugarResidencia" element={isAuthenticated ? <LugarResidencia /> : <Navigate to="/LugarResidencia" replace />} />
+            <Route path="/Grado" element={isAuthenticated ? <Grado /> : <Navigate to="/Grado" replace />} />
+            <Route path="/Matricula" element={isAuthenticated ? <Matricula /> : <Navigate to="/Matricula" replace />} />
+            <Route path="/Usuarios" element={isAuthenticated ? <Usuarios /> : <Navigate to="/Usuarios" replace />} />
+            <Route path="/Roles" element={isAuthenticated ? <Roles /> : <Navigate to="/Roles" replace />} />
+            <Route path="/Noticias" element={isAuthenticated ? <Noticias /> : <Navigate to="/Noticias" replace />} />
+            <Route path="/Parentesco" element={isAuthenticated ? <Parentesco /> : <Navigate to="/Parentesco" replace />} />
+            <Route path="/Materias" element={isAuthenticated ? <Materias /> : <Navigate to="/Materias" replace />} />
+            <Route path="/Examen" element={isAuthenticated ? <Examen /> : <Navigate to="/Examen" replace />} />
+            <Route path="/Tareas" element={isAuthenticated ? <Tareas /> : <Navigate to="/Tareas" replace />} />
+            <Route path="/Cotidiano" element={isAuthenticated ? <Cotidiano /> : <Navigate to="/Cotidiano" replace />} />
+            <Route path="/Asistencia" element={isAuthenticated ? <Asistencia /> : <Navigate to="/Asistencia" replace />} />
+            <Route path="/Justificacion" element={isAuthenticated ? <Justificacion /> : <Navigate to="/Justificacion" replace />} />
+            <Route path="/AdminDashboard" element={isAuthenticated ? <AdminDashboard /> : <Navigate to="/AdminDashboard" replace />} />
+            <Route path="/ProfesorDashboard" element={isAuthenticated ? <ProfesorDashboard /> : <Navigate to="/ProfesorDashboard" replace />} />
+            <Route path="/EstudianteDashboard" element={isAuthenticated ? <EstudianteDashboard /> : <Navigate to="/EstudianteDashboard" replace />} />
           </Routes>
         </ThemeProvider>
       </UserProvider>
